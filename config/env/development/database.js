1255702921b53d7fa9123dfcf9b2270b7f465559
anidast <anidwi.ad1@gmail.com>
2021-03-02 12:19:25 +0700

    Update database config

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -1,26 +1,16 @@
-const { parse } = require("pg-connection-string");
-
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
-
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "bookshelf",
-        settings: {
-          client: "sqlite",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-          ssl: { rejectUnauthorized: false }
-        },
-        options: {
-          ssl: false
-        },
+module.exports = ({ env }) => ({
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'mongoose',
+      settings: {
+        host: env('DATABASE_HOST'),
+        port: env.int('DATABASE_PORT'),
+        database: env('DATABASE_NAME'),
+        username: env('DATABASE_USERNAME'),
+        password: env('DATABASE_PASSWORD'),
       },
+      options: {},
     },
-  };
-};
+  },
+});
